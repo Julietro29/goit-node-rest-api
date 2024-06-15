1e1d22bdb39aeb0e2805d34d56aadf8abd3fe9f4
Julietro29 <mousemiu1@gmail.com>
2024-06-15 01:49:28 -0600

    Update app

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ import express from "express";
 import morgan from "morgan";
 import cors from "cors";
 import connectDB from "./db.js";
-
 import contactsRouter from "./routes/contactsRouter.js";
 
 const app = express();
